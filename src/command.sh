d37b67f58022c7984c4613f355da619dab059afc
rajiv gardas <gardasrajiv@gmail.com>
2023-05-31 16:28:24 -0500

    final changes

--- src/command.sh
+++ src/command.sh
@@ -1,15 +1,16 @@
-echo "npx create-react-app my-app" > command.sh
-echo "cd my-app" >> command.sh
-echo "git init" >> command.sh
-echo "git add ." >> command.sh
-echo "git commit -m 'Initial commit'" >> command.sh
-echo "gh repo create BLACKBIRD" >> command.sh
-echo "git checkout -b update_logo" >> command.sh
-echo "<Replace file content and link>" >> command.sh
-echo "git add ." >> command.sh
-echo "git commit -m 'Update logo and link'" >> command.sh
-echo "git push origin update_logo" >> command.sh
-echo "gh pr create --base master --head update_logo --title 'Update logo and link'" >> command.sh
-echo "gh pr merge 2 --auto" >> command.sh
-echo "echo 'REPO_URL https://github.com/rajivgardas0/BLACKBIRD' >> command.sh"
+"npx create-react-app my-app" 
+"cd my-app" 
+"git init" 
+"git add ." 
+"git commit -m 'Initial commit'" 
+"gh repo create BLACKBIRD" 
+"git checkout -b update_logo" 
+"<Replace logo with the provided url  and changed link in the app.js with provided url >" 
+"git add ." 
+"git commit -m 'Update logo and link'" 
+"git push origin update_logo"
+"gh pr create --base master --head update_logo --title 'Update logo and link'" 
+"gh pr merge 2 --auto" 
+
+REPO_URL https://github.com/rajivgardas0/BLACKBIRD'
 
